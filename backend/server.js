959f2e69ba5c5e9eb3d3b025374a7a6cc08edc90
BryanSantana <bryansantana0210@gmail.com>
2024-07-19 14:22:12 -0400

    added screens

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use('/api/families', familyRoutes);
 app.use('/api/announcements', announcementRoutes)
 app.use('/api/payments', paymentRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
